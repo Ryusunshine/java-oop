441e95452030b17aa79e6c00f4956090a06da5e4
ryusun <ryusunshine@gmail.com>
2022-12-23 03:24:39 +0900

    Implement SortService
    정렬 구현체를 사용하는 서비스를 별도로 작성
    의존성 주입 방법을 적용

--- java-oop/src/main/java/com/fastcampus/javaoop/JavaOopApplication.java
+++ java-oop/src/main/java/com/fastcampus/javaoop/JavaOopApplication.java
@@ -1,13 +1,17 @@
 package com.fastcampus.javaoop;
 
+import com.fastcampus.javaoop.logic.JavaSort;
+import com.fastcampus.javaoop.logic.Sort;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class JavaOopApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(JavaOopApplication.class, args);
+//    원래는 JavaSort<String> sort = new JavaSort<>(); 이거인데 interface를 만들면
+        Sort<String> sort = new JavaSort<>();
+        System.out.println("[result]" + sort.sort(Arrays.asList(args)));
     }
-
 }
---
+++ java-oop/src/main/java/com/fastcampus/javaoop/service/SortService.java
@@ -0,0 +1,22 @@
+package com.fastcampus.javaoop.service;
+
+import com.fastcampus.javaoop.logic.JavaSort;
+import com.fastcampus.javaoop.logic.Sort;
+
+import java.util.List;
+
+public class SortService {
+
+    private final Sort<String> sort; //필드에다가 주입하고싶은 애를 넣는다.
+
+    public SortService(Sort<String> sort){// 의존성 주입방식 = 생성자 주입
+        this.sort = sort;
+    }
+
+    public List<String> doSort(List<String> list){
+        Sort<String> sort = new JavaSort<>();
+
+        return sort.sort(list);
+    }
+    // 이 코드는 정렬 인터페이스를 가지고있긴 하지만 구체적으로 어떤 구현체를 가지고 정렬할것인지  모름
+}
